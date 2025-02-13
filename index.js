ca00d2393cf309d738d628d292164a6b348fd2a1
sundaramkumar <guyfromchennai@gmail.com>
2025-02-13 14:38:18 +0530

    refactor: remove debug logs and update readme with usage examples

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ class passwordGrid {
     if (this.alphaLower) str += ALPHA_LOWER_CHARS;
     if (this.numbers) str += NUMBER_CHARS;
     if (this.symbols) str += SYMBOL_CHARS;
-    console.log("str", str);
     return str;
   };
 
@@ -42,33 +41,21 @@ class passwordGrid {
 
   generateGrid() {
     var usableChars = this.getUsableChars();
-    console.log("usableChars", usableChars);
     var str = "";
-    // var headr = "+-------------------------------+\n";
     var headr = "+" + Array(this.gridSize * 4).join("-") + "+\n";
-    // var headr2 = "| Srvr:" + Array(this.gridSize * 3).join(" ") + "    |\n";
-    // headr2 += "| User:" + Array(this.gridSize * 3).join(" ") + "|\n";
-    // headr2 += "| Date:" + Array(this.gridSize * 3).join(" ") + "|\n";
-    // var divr = "|" + Array(this.gridSize * 4).join("-") + "|\n";
     var divr = "|" + Array(this.gridSize * 4).join("-") + "|\n";
 
-    // for (var x = 0; x < 3; x++) {
-    // str += headr + headr2;
     str += headr;
     for (var y = 0; y < this.gridSize; y++) {
-      // for (var z = 0; z < 3; z++) {
       for (var c = 0; c < this.gridSize; c++) {
         str += "| " + this.randomChar(usableChars) + " ";
         if (c == this.gridSize - 1) str += "|";
       }
-      // if (z == 2) str += "\n";
-      // }
       if (y != this.gridSize - 1) str += "\n" + divr;
       if (y == this.gridSize - 1) str += "\n";
     }
 
     str += headr;
-    // }
 
     return str;
   }
--- readme.md
+++ readme.md
@@ -1,3 +1,50 @@
 # PasswordGrid
 
 NPM Package to Generate Password Grid
+
+## Usage
+
+```javascript
+const passwordGrid = require("passwordGrid");
+var grid = new passwordGrid();
+var gridStr = grid.generateGrid();
+console.log(gridStr);
+```
+
+This will generate a password grid similar to
+
+```
++-------------------------------+
+| | | ! | e | p | : | { | : | 0 |
+|-------------------------------|
+| Y | < | R | \ | ^ | , | A | _ |
+|-------------------------------|
+| W | X | I | q | % | g | = | l |
+|-------------------------------|
+| 1 | d | g | _ | K | > | a | & |
+|-------------------------------|
+| @ | < | + | e | ] | [ | K | $ |
+|-------------------------------|
+| 9 | , | % | S | w | 2 | J | 2 |
+|-------------------------------|
+| D | 3 | M | 0 | t | E | 7 | X |
+|-------------------------------|
+| ! | E | H | - | I | b | 6 | Z |
++-------------------------------+
+```
+
+### Grid Configuration
+
+While generating grid, you can configure the grid generation parameters.
+
+```javascript
+grid.gridConfig({
+  numbers: false,
+  alphaUpper: false,
+  alphaLower: true,
+  symbols: true,
+  gridSize: 8,
+});
+```
+
+Max gridSize is 16 and min is 8.
